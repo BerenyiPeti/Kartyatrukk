70224ff3b4b8eca6d450e47962eee278518b536c
BerenyiPeti <berenyipeti01@gmail.com>
2022-09-10 21:37:20 +0200

    alap metódusok

---
+++ src/kartyatrukk/Main.java
@@ -0,0 +1,41 @@
+package kartyatrukk;
+
+public class Main {
+
+    public static String[] pakli = {"P_ÁSZ", "P_KIR", "P_FEL", "P_X", "P_IX",
+        "P_VIII", "T_ÁSZ", "T_KIR", "T_FEL", "T_X", "T_IX", "T_VIII", "Z_ÁSZ", "Z_KIR",
+        "Z_FEL", "Z_X", "Z_IX", "Z_VIII", "M_ÁSZ", "M_KIR", "M_FEL"};
+    public static String[] sor1 = new String[7];
+    public static String[] sor3 = new String[7];
+    public static String[] sor2 = new String[7];
+
+    public static void main(String[] args) {
+        feltolt();
+        sorokKirak();
+        melyik();
+        kever();
+        valasztottLap();
+
+    }
+
+    public static void feltolt() {
+
+    }
+
+    public static void sorokKirak() {
+
+    }
+
+    public static void kever() {
+
+    }
+
+    private static void melyik() {
+
+    }
+
+    private static void valasztottLap() {
+
+    }
+
+}
